477ff83a27d4d395574fb3ccb3f5470652bdc93a
zhengxingfu <2509699229@qq.com>
2021-07-08 19:43:28 +0800

    学习到了函数function

--- test.go
+++ test.go
@@ -4,6 +4,7 @@ package main
 //导入的包
 import (
 	"fmt"
+	"sort"
 	"strconv"
 )
 
@@ -201,7 +202,117 @@ LABEL1: //标签
 	fmt.Println("切片的信息", len(s2), cap(s2))
 	fmt.Println("切片内容", s2)
 
+	bb1 := []int{0, 1, 2, 3, 4, 5, 6, 7}
+	s3 := bb1[2:5]
+	fmt.Println(s3, len(s3), cap(s3))
+	//个数就是3，容量则是从三开始到最后是7 - 2 + 1
+	s3 = append(s3, 1, 2)
+	fmt.Println("追加后", s3)
+	//数组拷贝,把后一个数组copy给前一个数组。其中长度以短的为准
+	sCopy1 := []int{1, 2, 3, 4, 5, 6}
+	sCopy2 := []int{10, 12, 77}
+	copy(sCopy2, sCopy1)
+	fmt.Println("copy后的数组", sCopy2)
+
+	//map
+	//创建map
+	m := make(map[int]string)
+	m[1] = "ok11" //赋值
+	delete(m, 1)  //删除键值对
+	fmt.Println("简单map", m[1])
+	//map嵌套map需要注意每一级的map都需要单独的初始化
+	var mm2 map[int]map[int]string
+	mm2 = make(map[int]map[int]string)
+	aa2, isOk := mm2[1][1] //第二层为bool代表是否存在这个键值对
+	//fmt.Println(isOk)
+	if !isOk {
+		mm2[1] = make(map[int]string) //初始化第二层map
+	}
+	mm2[1][1] = "seven"
+	aa2, isOk = mm2[1][1]
+	fmt.Println("复杂map", aa2, isOk)
+	//map的迭代操作 range..两参数i，j  如果迭代的是map，则第一个参数返回的是每一个map的key
+	sm := make([]map[int]string, 5) //以map为类型的切片slice，五个元素
+	for key := range sm {           //这里面的第二个参数只是拷贝，不会对原来的造成任何影响..第一个参数是下标
+		/*v = make(map[int]string, 1)	//初始化map
+		v[1] = "ok"
+		fmt.Println(v)*/
+		sm[key] = make(map[int]string, 1)
+		sm[key][1] = "okk"
+		fmt.Println(sm[key])
+	}
+	fmt.Println(sm)
+	//map的间接排序
+	mm3 := map[int]string{1: "aa", 2: "bbb", 7: "ccc", 4: "ddd", 5: "eee"}
+	ss3 := make([]int, len(mm3))
+	ii3 := 0
+	for key, _ := range mm3 {
+		ss3[ii3] = key
+		ii3++
+	}
+	sort.Ints(ss3)
+	fmt.Println("go是无序的", ss3)
+	/**作业：
+	将map[int]string类型的一组map 键和值交换，变成类型map[string]int
+	*/
+	map1Homework := map[int]string{1: "aa", 2: "bbb", 7: "ccc", 4: "ddd", 5: "eee"}
+	map2HomeWork := make(map[string]int)
+	for key, val := range map1Homework {
+		map2HomeWork[val] = key
+	}
+	fmt.Println("转换类型后", map2HomeWork)
+	//myFunc3(1, 2, 3, 4, 5)
+	//匿名函数,把这个函数作为一个类型传递给变量
+	aaFunc := func() {
+		fmt.Println("这是匿名函数")
+	}
+	aaFunc()
+	//闭包的使用
+	funcClose1 := clouure(3)           //先定义一个变量接受闭包函数返回的匿名函数
+	fmt.Println("使用闭包", funcClose1(4)) //使用闭包
+
+	/**
+	defer
+	在函数体执行结束后，按着调用顺序的 **相反顺序** 逐个执行
+	*/
+	/*fmt.Println("以下是defer，逆序向上调用", "a")
+	defer fmt.Println("b")
+	defer fmt.Println("c")*/
+	//for循环来使用defer
+	for i := 0; i < 3; i++ {
+		defer fmt.Println(i)
+	}
+	//defer的高级调用
+
 }
 func test() {
 	const Name = "123"
+}
+
+//定义自己的函数function
+//两个参数，返回一个int
+func myFunc(a int, b string) int {
+	return 0
+}
+
+//参数a和b都是int类型，返回类型是int和string两个
+func myFunc1(a, b int) (int, string) {
+	return int(1), string("test")
+}
+
+//命名返回值，若使用了命名返回值，则return就可以
+func myFunc2(a int) (d, b, c int) {
+	return d, b, c
+}
+
+//go语言的不定长变参
+func myFunc3(a ...int) {
+	fmt.Println("调用了函数", a)
+}
+
+//闭包函数，作用是返回一个匿名函数
+func clouure(x int) func(int) int {
+	return func(y int) int {
+		return x + y
+	}
 }
